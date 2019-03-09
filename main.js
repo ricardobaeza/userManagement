5e951d2e4fdb6338f69a0f60ee8ec2e31a94e7b4
Ricardo Baeza <baezaric3600@gmail.com>
2019-03-09 12:20:34 -0700

    fixed a small bug

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use((express.urlencoded({extended: false})));
 app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
-    res.render('index');
+    res.redirect('/addUser');
 });
 
 
